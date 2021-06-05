5ae51aefc113b9b391ba3bf0f7c5de17470e5df3
Mark Visschers <visschers.m@gmail.com>
2021-06-05 17:14:13 +0200

    first commit

---
+++ MongoDatabaseExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using MongoDB.Driver;
+using System.Collections.Generic;
+
+namespace MPTech.Mongo.Extensions
+{
+    public static class MongoDatabaseExtensions
+    {
+        public static List<string> CollectionNamesAsList(this IMongoDatabase database)
+        {
+            return database.ListCollectionNames().ToList();
+        }
+
+        public static void DropAllCollections(this IMongoDatabase database, params string[] collectionNames)
+        {
+            if (collectionNames.Any())
+            {
+                collectionNames.ToList()
+                    .ForEach(collectionName => database.DropCollection(collectionName));
+
+                return;
+            }
+
+            database.CollectionNamesAsList()
+                .ForEach(collectionName => database.DropCollection(collectionName));
+        }
+    }
+}
