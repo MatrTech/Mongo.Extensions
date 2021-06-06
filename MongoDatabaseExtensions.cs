c8eecf1673026204a8d9befd610b41d68e0d16eb
Pjotr Casteel <pjotr_2@live.nl>
2021-06-06 12:46:18 +0200

    Added summary documentation of all methods

--- MongoDatabaseExtensions.cs
+++ MongoDatabaseExtensions.cs
@@ -7,23 +7,38 @@ namespace MPTech.Mongo.Extensions
 {
     public static class MongoDatabaseExtensions
     {
+        /// <summary>
+        /// Creates a <see cref="List{string}"/> of all collections in the given <paramref name="database"/>.
+        /// </summary>
+        /// <param name="database"></param>
+        /// <returns>A <see cref="List{string}"/> that contains all collection names in the given <paramref name="database"/>.</returns>
         public static List<string> CollectionNamesAsList(this IMongoDatabase database)
         {
             return database.ListCollectionNames().ToList();
         }
 
+        /// <summary>
+        /// Creates a <see cref="Task{List{string}}"/> of all collections in the given <paramref name="database"/>.
+        /// </summary>
+        /// <param name="database"></param>
+        /// <returns>A <see cref="Task{List{string}}"/> that contains all collection names in the given <paramref name="database"/>.</returns>
         public static async Task<List<string>> CollectionNamesAsListAsync(this IMongoDatabase database)
         {
             return (await database.ListCollectionNamesAsync()).ToList();
         }
 
+        /// <summary>
+        /// Droppes all collections in the given <paramref name="collectionNames"/> if left empty, it will remove all collections in the given <paramref name="database"/>.
+        /// </summary>
+        /// <param name="database"></param>
+        /// <param name="collectionNames"></param>
         public static void DropAllCollections(this IMongoDatabase database, params string[] collectionNames)
         {
             if (collectionNames.Any())
             {
                 collectionNames.ToList()
                     .ForEach(collectionName => database.DropCollection(collectionName));
-
+                
                 return;
             }
 
@@ -31,6 +46,11 @@ namespace MPTech.Mongo.Extensions
                 .ForEach(collectionName => database.DropCollection(collectionName));
         }
 
+        /// <summary>
+        /// Droppes all collections in the given <paramref name="collectionNames"/> if left empty, it will remove all collections in the given <paramref name="database"/>.
+        /// </summary>
+        /// <param name="database"></param>
+        /// <param name="collectionNames"></param>
         public static async Task DropAllCollectionsAsync(this IMongoDatabase database, params string[] collectionNames)
         {
             if (collectionNames.Any())
