e206fadad3fca99f9eea73b776b15a9b8afb3f2e
Mark Visschers <visschers.m@gmail.com>
2021-06-19 19:46:10 +0200

    restructured project

---
+++ tests/Utilities.Extensions.Mongo.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Utilities.Extensions.Mongo.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
