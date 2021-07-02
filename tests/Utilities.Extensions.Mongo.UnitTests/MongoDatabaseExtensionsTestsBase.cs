f3d4056560b63922b801dd5caa7724926f239e0c
Mark Visschers <mark.visschers@connecthing.tech>
2021-07-02 15:53:19 +0200

    splitted test files and added new tests and implementation of MongoDatabaseExtensions CollectionExists method

---
+++ tests/Utilities.Extensions.Mongo.UnitTests/MongoDatabaseExtensionsTestsBase.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MongoDB.Driver;
+
+namespace MatrTech.Utilities.Extensions.Mongo.UnitTests
+{
+    [TestClass]
+    public class MongoDatabaseExtensionsTestsBase
+    {
+        protected static MongoClient client = null!;
+        protected IMongoDatabase? database;
+        protected IMongoDatabase? otherDatabase;
+
+        public MongoDatabaseExtensionsTestsBase()
+        {
+            client = new MongoClient("mongodb://localhost:27017");
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            client.DropAllDatabases();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            database?.DropAllCollections();
+            otherDatabase?.DropAllCollections();
+        }
+    }
+}
